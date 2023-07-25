60b108b3c44423ce943c5dfb68ddb75d9fa3a589
faladetimilehin <faladetimilehin@yahoo.com>
2023-07-25 16:03:41 +0100

    Creating the first javascript for random password generator

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
-# random-password-generator
+# RANDOM PASSWORD GENERATOR
+
+## Description
+
+creating an application that an employee can use to generate a random password based on criteria they’ve selected. This app will run in the browser, and will feature dynamically updated HTML and CSS powered by JavaScript code. It has a clean and polished user interface that is responsive, ensuring that it adapts to multiple screen sizes.
+
+## Summary
+
+- [Description](#description)
+- [User Story](#user-story)
+- [Acceptance Criteria](#acceptance-criteria)
+- [Mock-Up](#mock-up)
+- [Technologies Used](#technologies-used)
+- [Usage](#usage)
+- [Deployment](#deployment)
+
+### User Story
+
+```
+* Generate a password when the button is clicked
+  * Present a series of prompts for password criteria
+    * Length of password
+      * At least 8 characters but no more than 128.
+    * Character types
+      * Lowercase
+      * Uppercase
+      * Numeric
+      * Special characters ($@%&*, etc)
+  * Code should validate for each input and at least one character type should be selected
+  * Once prompts are answered then the password should be generated and displayed in an alert or written to the page
+```
+
+### Acceptance Criteria
+
+- Create a portfolio based on bootstrap
+- Use media queries to create a portfolio across different screens
+
+### Mock-Up
+
+The following image shows the web application's appearance and functionality:
+
+![password generator demo](./assets/05-javascript-challenge-demo.png)
+
+### Technologies Used
+
+- HTML
+- CSS
+- JAVASCRIPT
+
+### Usage
+
+You can clone this repository using `git clone https://github.com/faladetimilehin/random-password-generator` or ssh `git clone git@github.com:faladetimilehin/random-password-generator.git`
+
+### Deployment
+
+https://faladetimilehin.github.io/random-password-generator/
---
+++ script.js
@@ -0,0 +1,162 @@
+// Array of special characters to be included in password
+var specialCharacters = [
+  '@',
+  '%',
+  '+',
+  '\\',
+  '/',
+  "'",
+  '!',
+  '#',
+  '$',
+  '^',
+  '?',
+  ':',
+  ',',
+  ')',
+  '(',
+  '}',
+  '{',
+  ']',
+  '[',
+  '~',
+  '-',
+  '_',
+  '.'
+];
+
+// Array of numeric characters to be included in password
+var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+// Array of lowercase characters to be included in password
+var lowerCasedCharacters = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z'
+];
+
+// Array of uppercase characters to be included in password
+var upperCasedCharacters = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z'
+];
+
+var choiceArr = [];
+
+// Function to prompt user for password options
+function getPasswordOptions() {
+
+  var specialCharacter = confirm('Click OK to confirm including special characters');
+  var numericCharacter = confirm('Click OK to confirm including numeric characters');
+  var lowerCasedCharacter = confirm('Click OK to confirm including lowercase characters');
+  var upperCasedCharacter = confirm('Click OK to confirm including uppercase characters');
+  choiceArr += specialCharacter ? choiceArr.concat(specialCharacters) : '';
+  console.log(choiceArr)
+  choiceArr += numericCharacter ? choiceArr.concat(numericCharacters) : '';
+  console.log(choiceArr)
+  choiceArr += lowerCasedCharacter ? choiceArr.concat(lowerCasedCharacters) : '';
+  console.log(choiceArr)
+  choiceArr += upperCasedCharacter ? choiceArr.concat(upperCasedCharacters) : '';
+  console.log(choiceArr)
+  console.log(`specialCharacter:${specialCharacter}`,
+    `numericCharacter:${numericCharacter}`,
+    `lowerCasedCharacter:${lowerCasedCharacter}`,
+    `upperCasedCharacter:${upperCasedCharacter}`
+  )
+  console.log(choiceArr)
+  if (choiceArr.length < 0) {
+    alert('Please enter one valid character')
+  }
+}
+// Function for getting a random element from an array
+function getRandom(arr, charactersLength) {
+  var randomPassword = ''
+  console.log(arr)
+  for (var i = 0; i < charactersLength; i++) {
+    console.log(arr[i])
+    console.log(Math.floor(Math.random() * arr.length))
+    var randomPosition = Math.floor(Math.random() * arr.length)
+    randomPassword = randomPassword + choiceArr[randomPosition]
+  }
+  console.log(randomPassword)
+  return randomPassword
+}
+
+// Function to generate password with user input
+function generatePassword() {
+
+  var password = ''
+  console.log("Generating password");
+  var charactersLength = parseInt(prompt('How many characters would you like your password to contain?'))
+  console.log(charactersLength, 'characters');
+
+  if (charactersLength > 128 || isNaN(charactersLength) || charactersLength < 8) {
+    alert('Password length cannot be less than 8 and more than 128 characters and enter a number')
+    return false
+  } else {
+    getPasswordOptions();
+    getRandom(choiceArr, charactersLength)
+  }
+
+}
+
+// Get references to the #generate element
+var generateBtn = document.querySelector('#generate');
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector('#password');
+
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener('click', writePassword);
